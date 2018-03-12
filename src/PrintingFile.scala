e606a6749c9c943e3877edbede20c21e6d9ecab3
KraFourZee <disandrokyle@gmail.com>
2018-03-12 17:41:03 -0600

    updated PrintingFile.. technically fib

--- src/PrintingFile.scala
+++ src/PrintingFile.scala
@@ -1,16 +1,9 @@
 import scala.io.Source
 import java.io.{FileNotFoundException, IOException}
 
-object PrintingFile {
-  def main(args: Array[String]){
-    val filename = "../random.txt" // error handling on file that doesn't exist.. if you want to see this change it to something wrong!
-	try {
-		for (line <- Source.fromFile(filename).getLines) {
-			println(line)
-		}
-	} catch {
-		case e: FileNotFoundException => println("Couldn't find that file.")
-		case e: IOException => println("Got an IOException!")
-	}
-  }
+object PrintingFile extends App {
+  //def main(args: Array[String]){
+		val fibs: Stream[Int] = 0 #:: fibs.scanLeft(1)(_ + _)
+		println(fibs.take(10).toList)
+	//}
 }
