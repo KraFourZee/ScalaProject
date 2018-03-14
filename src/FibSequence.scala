0eeb609ea41c26fb89c38b7f34102bae9cba35cf
KraFourZee <disandrokyle@gmail.com>
2018-03-14 15:28:39 -0600

    Fib Sequence

--- src/FibSequence.scala
+++ src/FibSequence.scala
@@ -1,7 +1,7 @@
 import scala.io.Source
 import java.io.{FileNotFoundException, IOException}
 
-object PrintingFile extends App {
+object FibSequence extends App {
   //def main(args: Array[String]){
 		val fibs: Stream[Int] = 0 #:: fibs.scanLeft(1)(_ + _)
 		println(fibs.take(10).toList)
